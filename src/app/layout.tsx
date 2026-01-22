428f2fa9a78a67f05cc29061a4785309cc0ffd56
Jakub Jęsiek <jesiekuba@gmail.com>
2026-01-22 06:41:16 +0100

    feat(src/app/layout.tsx): Added the script to root Google Maps layout so it loads globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Maps Script loaded with beforeInteractive strategy */}
+        <Script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+          strategy="beforeInteractive"
+        />
         {children}
       </body>
     </html>
