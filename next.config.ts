3ea32b3e7ffc4e9beae09ef2c8453f81c1493122
Jakub Jęsiek <jesiekuba@gmail.com>
2026-01-22 07:53:25 +0100

    feat(next.config.ts): Increased the bodySizeLimit to 50 MB

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
+  // We also need to authorize image domains for the map details page later
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allow Supabase images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
