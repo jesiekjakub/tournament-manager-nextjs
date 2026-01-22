9479259ae049b9b13de25cd67dcc805f4c545fc4
Jakub Jęsiek <jesiekuba@gmail.com>
2026-01-22 06:22:36 +0100

    rename(src/middleware.ts -> src/proxy.ts): Changed the middleware filename to comly to the current Next standard

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // 1. Run the Supabase session update
   // This reads the cookie from the request and ensures it's refreshed on the response
   return await updateSession(request)
